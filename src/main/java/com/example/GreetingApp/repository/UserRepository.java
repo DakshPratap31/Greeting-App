0f5c12f1706c579b405e47d52651dbbd5c056cc7
Daksh pratap <daksh.pratap_cs21@gla.ac.in>
2025-03-05 21:54:24 +0530

    user registration added

---
+++ src/main/java/com/example/GreetingApp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.GreetingApp.repository;
+
+import com.example.GreetingApp.model.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<AuthUser, Long> {
+
+
+}
